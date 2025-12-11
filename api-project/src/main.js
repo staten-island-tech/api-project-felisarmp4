07a3208e4d72fa396383903127df97efbb50a0c0
felisarmp4 <felisayereyes@gmail.com>
2025-12-11 14:03:27 -0500

    asdasdas

--- api-project/src/main.js
+++ api-project/src/main.js
@@ -1,6 +1,18 @@
 import "./style.css";
 
-fetch("http://ponyapi.net/v1/song/all")
+fetch("http://ponyapi.net/v1/song/all?limit=192")
   .then((response) => response.json())
   .then((data) => console.log(data));
 console.log();
+
+async function getsong(song) {
+  try {
+    const response = await fetch(song);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+getsong(ACADECA);
