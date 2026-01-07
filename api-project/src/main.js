19e69d8efcc8b8a8e6183c13ab134f907e63d520
felisarmp4 <felisayereyes@gmail.com>
2026-01-07 14:03:06 -0500

    sadsad

--- api-project/src/main.js
+++ api-project/src/main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 
 const url = "http://ponyapi.net/v1/song/all?limit=192";
+const songcarddiv = document.querySelector('#songcarddiv');
 
 async function getsong(url) {
   try {
@@ -20,9 +21,18 @@ export async function loadsong(url){
     const data = await response.json();
     const dataarr = data['data'];
     dataarr.forEach(obj => {
-      if(!obj.hasOwnProperty('images')){
+      const songname = obj['name']
+      if(!obj.hasOwnProperty('video')){
         return;
       }
+      const songimg = obj['video'][0]
+      songcarddiv.insertAdjacentHTML('beforeend',
+        `
+        <div class="card w-full"></div>
+        <p class="songname text center text-4x1 mt-2 mb-2">${songname}</p>
+        
+        `
+      )
     })
   }
 }
