cce73d04cae788eb515ddb85ce9693da1fad824f
felisarmp4 <felisayereyes@gmail.com>
2026-01-08 14:02:43 -0500

    fwefwef

--- api-project/src/main.js
+++ api-project/src/main.js
@@ -1,38 +1,55 @@
 import "./style.css";
 
 const url = "http://ponyapi.net/v1/song/all?limit=192";
-const songcarddiv = document.querySelector('#songcarddiv');
+const songcarddiv = document.querySelector("#songcarddiv");
 
-async function getsong(url) {
+async function loadsong(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
-getsong(url);
+    const songs = data.data;
 
-export async function loadsong(url){
-  try{
-    const response = await fetch(url);
-    const data = await response.json();
-    const dataarr = data['data'];
-    dataarr.forEach(obj => {
-      const songname = obj['name']
-      if(!obj.hasOwnProperty('video')){
-        return;
-      }
-      const songimg = obj['video'][0]
-      songcarddiv.insertAdjacentHTML('beforeend',
+    songcarddiv.innerHTML = "";
+
+    songs.forEach((song) => {
+      const name = song.name;
+      const episode = song.episode;
+      const keysignature = song.keysignature;
+      const length = song.length;
+      const lyricsby = song.lyricsby;
+      const musicby = song.musicby;
+      const video = song.video;
+
+      if (!song.video) return;
+      const embedURL = song.video.replace("watch?v=", "embed/");
+      songcarddiv.insertAdjacentHTML(
+        "beforeend",
         `
-        <div class="card w-full"></div>
-        <p class="songname text center text-4x1 mt-2 mb-2">${songname}</p>
-        
+      <div class="w-200 mx-auto rounded-2xl m-3 text-3xl text-indigo-900 text-center">
+      <h2>${name}</h2>
+      <p>${episode}</p>
+      <p>Key: ${keysignature}</p>
+      <p>Length: ${length}</p>
+      <p>Lyrics: ${lyricsby}</p>
+      <p>Music: ${musicby}</p>
+      <a href="${video}" target="_blank">Watch video</a>
+      </div>
+
+         <iframe 
+            width="560" 
+            height="315" 
+            src="${embedURL}" 
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowfullscreen>
+          </iframe>
+        </div>
         `
-      )
-    })
+      );
+    });
+  } catch (error) {
+    console.error("failed to load songs:", error);
   }
 }
+
+loadsong(url);
